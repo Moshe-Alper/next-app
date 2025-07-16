6e5f738898a52b45b23e325b1e2f17f11dac164f
Moshik Alper <moshikalper@gmail.com>
2025-07-16 19:27:17 +0300

    Add fetching users from api and add to cart button

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('add')}>Add to Cart</button>
+    </div>
+  )
+}
+
+export default AddToCart
