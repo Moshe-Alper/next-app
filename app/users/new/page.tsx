7531acf15a2f1fc4a44a2f0a37e4fe8d0bfad086
Moshik Alper <moshikalper@gmail.com>
2025-08-02 16:09:51 +0300

    Add New Users page and link to it from Users page

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,8 +1,14 @@
+'use client'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 function NewUsersPage() {
+
+  const router = useRouter()
   return (
-    <div>NewUsersPage</div>
+    <button 
+    className='btn btn-primary'
+    onClick={() => router.push('/users')}>Create</button>
   )
 }
 
