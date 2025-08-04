cb63f28969a3b9783914a1ebe91caa29fe04dcd8
Moshik Alper <moshikalper@gmail.com>
2025-08-04 16:09:47 +0300

    Implement user retrieval and creation endpoints in API

---
+++ app/api/users/route.tsx
@@ -0,0 +1,17 @@
+import { error } from "console";
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(request: NextRequest) {
+    return NextResponse.json([
+        { id: 1, name: 'Popo'},
+        { id: 2, name: 'Momo'},
+    ])
+}
+
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+    if (!body.name) {
+        return NextResponse.json({ error: 'Name is required' }, { status: 400})
+    }
+    return NextResponse.json({ id: 1, name: body.name }, { status: 201 })
+}
