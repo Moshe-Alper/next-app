3e105434e39e8d8adb8b58b2aaa270dea7e650cd
Moshik Alper <moshikalper@gmail.com>
2025-08-06 15:00:11 +0300

    Refactor user API route to enhance PUT and DELETE methods with proper error handling and response structure

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -15,3 +15,35 @@ export function GET(
     }
     return NextResponse.json({ id: 1, name: 'Popo'})
 }
+
+export async function PUT(
+    request: NextRequest,
+    { params }: { params: { id: number } }) {
+    // Validate the request body
+    // If invalid, return 400
+    // Fetch the user with the given id
+    // If doesn't exist, return 404
+    // Update the user
+    // Return the update user
+    const body = await request.json()
+    if (!body.name) {
+        return NextResponse.json({ error: 'Name is required' }, { status: 400})
+    }
+    if (params.id > 10) {
+        return NextResponse.json({ error: 'User not found' }, { status: 404})
+    }
+    return NextResponse.json({ id: 1, name: body.name })
+}
+
+export function DELETE(
+    request: NextRequest,
+    { params }: { params: { id: number } }) { 
+    // Fetch user from db
+    // If not found, return 404
+    // Delete the user
+    // Return 200
+    if (params.id > 10) {
+        return NextResponse.json({ error: 'User not found' }, { status: 404})
+    }
+        return NextResponse.json({})
+}
