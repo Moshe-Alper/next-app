38b222c1354b7b4a22895ae8ffd652b157735777
Moshik Alper <moshikalper@gmail.com>
2025-08-17 14:09:32 +0300

    Add email sending functionality with welcome template
    - Updated package.json to include new dependencies for email functionality
    - Created API route for sending emails using Resend service
    - Added WelcomeTemplate component for email content

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,29 @@
+import React, { CSSProperties } from 'react'
+import { Html, Body, Container, Tailwind, Text, Link, Preview } from '@react-email/components'
+
+const WelcomeTemplate = ({ name}: {name: string }) => {
+  return (
+    <Html>
+        <Preview>Welcome aboard!</Preview>
+        <Tailwind>
+        <Body>
+            <Container>
+                <Text className='font-bold text-3xl'>Hello {name}</Text>
+                <Link href="https://www.google.com">www.google.com</Link>
+            </Container>
+        </Body> 
+        </Tailwind>
+    </Html>
+  )
+}
+
+const body: CSSProperties = {
+    background: '#fff',
+}
+
+const heading: CSSProperties = {
+    fontSize: '32px'
+}
+
+export default WelcomeTemplate
+
