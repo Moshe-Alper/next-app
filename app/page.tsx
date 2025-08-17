9608b1eceabc01d3e988f20347eb2405195c2765
Moshik Alper <moshikalper@gmail.com>
2025-08-17 14:34:16 +0300

    Add image component and configure remote image patterns in Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,12 @@ export default async function Home() {
       <h1>Hello { session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
+      <Image 
+        src="https://images.unsplash.com/photo-1501594907352-04cda38ebc29" 
+        alt="Random nature image"
+        width={800}
+        height={600}
+      />    
     </main>
   )
 }
