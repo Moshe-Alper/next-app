5fdbe19ab4615afe15e0320f023987c7ac7fc16c
Moshik Alper <moshikalper@gmail.com>
2025-08-19 15:59:14 +0300

    Final changes

---
+++ app/components/HeavyComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeavyComponents = () => {
+  return (
+    <div>My Heavy Component</div>
+  )
+}
+
+export default HeavyComponents
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 import ProductCard from './components/ProductCard/ProductCard'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
-import { Metadata } from 'next'
-
+import HeavyComponents from './components/HeavyComponent'
+ 
 export default async function Home() {
   const session = await getServerSession(authOptions)
 
@@ -13,12 +13,13 @@ export default async function Home() {
       <h1>Hello { session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
-      <Image 
+      <HeavyComponents />
+      {/* <Image 
         src="https://images.unsplash.com/photo-1501594907352-04cda38ebc29" 
         alt="Random nature image"
         width={800}
         height={600}
-      />    
+      />     */}
     </main>
   )
 }
